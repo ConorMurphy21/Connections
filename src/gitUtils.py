05b8e3997ec00feb1603f93184c628d1adfaff2e
Conor <imuniquest@gmail.com>
2024-04-20 21:39:15 -0700

    minor fixes

--- src/gitUtils.py
+++ src/gitUtils.py
@@ -21,12 +21,12 @@ def save_to_git(args, user_config, file):
     subprocess.call(['git', 'pull', '--rebase', '--autostash', '-q'], cwd=DATA_PATH)
     subprocess.call(['git', 'add', file, '-q'], cwd=DATA_PATH)
     if args.generate:
-        subprocess.call(['git', 'commit', '-qm', f'{user_config.username} generated new game!'], cwd=DATA_PATH)
+        subprocess.call(['git', 'commit', '-q', '-m', f'{user_config.username} generated new game!'], cwd=DATA_PATH)
     else:
-        subprocess.call(['git', 'commit', '-qm', f'{user_config.username} played a game!'], cwd=DATA_PATH)
+        subprocess.call(['git', 'commit', '-q', '-m', f'{user_config.username} played a game!'], cwd=DATA_PATH)
 
     subprocess.run(['git', 'push'], cwd=DATA_PATH)
-    result = subprocess.run(['git', 'status'], cwd=DATA_PATH, check_output=True, text=True).stdout
+    result = subprocess.run(['git', 'status'], cwd=DATA_PATH, capture_output=True, text=True).stdout
     if 'nothing to commit' not in result:
         print('Something went wrong syncing with the cloud :(')
         print('Make sure conor gave you access to the repo')
