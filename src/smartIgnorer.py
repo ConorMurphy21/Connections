6f0aac28ec0b0c9ed65e5066d4d83a5b5afc29f3
conor <conormurphyx@gmail.com>
2024-04-24 11:35:17 -0700

    * add working smart ignorer, still maybe needs some tweaks to the constants

--- src/smartIgnorer.py
+++ src/smartIgnorer.py
@@ -1,4 +1,11 @@
+import logging
+import math
+import random
 import time
+
+DEFAULT_STD = 0.1
+MAX_INTERVAL = 1.2
+MIN_TIMEOUT = 0.3
 
 
 # a class to ignore keyboard input, but only when it keyboard input should be ignored
@@ -7,37 +14,65 @@ class SmartIgnorer:
     def __init__(self):
         # the word that was just selected (this is the time we possibly ignore keyboard input)
         self.just_selected = ''
-        # the cumulative average time between characters typed
-        self.cum_interval_average = 0
-        # the number of samples for the cumulative average
-        self.cum_n = 0
-        # the average time
-        self.current_interval_average = 0
-        self.current_n = 0
+        self.index = -1
+        # the cumulative mean time between characters typed and std
+        self.mean = -1
+        self.count = 0
+        self.m2 = 0
         # the last timestamp
         self.last_time = 0
 
-    def add_interval(self, word_start: bool, just_selected=''):
+    # welford's online algorithm for cumulative variance
+    def update_stats(self, new_value):
+        self.count += 1
+        if self.mean <= 0:
+            self.mean = new_value
+        delta = new_value - self.mean
+        self.mean += delta / self.count
+        delta2 = new_value - self.mean
+        self.m2 += delta * delta2
+
+    def _std(self):
+        if self.count < 2:
+            return DEFAULT_STD
+        return math.sqrt(self.m2 / self.count)
+
+    def _get_interval(self):
+        new_last_time = time.time()
+        interval = new_last_time - self.last_time
+        self.last_time = new_last_time
+        # avoid massive outliers bringing the average way up
+        return min(interval, MAX_INTERVAL)
+
+    def add_interval(self, word_start: bool, just_selected='', selected_index=-1):
+
         self.just_selected = just_selected
+        self.index = selected_index
         if word_start:
-            # add current avg to cumulative and reset current
-            # avoid possible divide by 0
-            if self.cum_n + self.current_n > 0:
-                self.cum_interval_average = (self.current_interval_average * self.current_n +
-                                             self.cum_interval_average * self.cum_n) / (self.cum_n + self.current_n)
-            self.cum_n += self.current_n
-            self.current_interval_average = 0
-            self.current_n = 0
             self.last_time = time.time()
             return
+        self.update_stats(self._get_interval())
 
-        interval = time.time() - self.last_time
-        self.last_time = time.time()
-        interval = max(interval, 1200)  # avoid massive outliers bringing the average way up
-        self.current_interval_average = (self.current_interval_average * self.current_n + interval) \
-                                        / (self.current_n + 1)
-        self.current_n += 1
-
-    def should_ignore(self, ch) -> bool:
+    def should_ignore(self, ch: int) -> bool:
+        # we should only ignore after a word was selected
         if self.just_selected == '':
             return False
+        # any non-alpha character implies intent and therefore we should not ignore
+        if not (ord('A') <= ch <= ord('Z') or ord('a') <= ch <= ord('z')):
+            return False
+        interval = self._get_interval()
+
+        ch = chr(ch).upper()
+        # if the character is the continuing character in the string they selected
+        if self.index < len(self.just_selected) and self.just_selected[self.index] == ch:
+            self.index += 1
+            # looks unlikely they are continuing word, even though
+            if interval > self.mean + 3.2 * self._std():
+                return False
+
+            return True
+        else:
+            # anything within MIN_TIMEOUT is probably an accident
+            if interval < MIN_TIMEOUT:
+                return True
+        return False
